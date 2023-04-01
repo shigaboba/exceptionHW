37a200c8c7e5fd75079334318caaae43dea8ae75
shigaboba <shigapovalrm@gmail.com>
2023-04-01 22:23:42 +0300

    Initial

---
+++ src/main/java/com/example/exceptionhw/ExceptionhwApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionhwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exceptionhw/ExceptionhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionhwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
