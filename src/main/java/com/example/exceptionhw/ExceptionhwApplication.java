72486af980b2b1aef359aac4a9be4f78462515c9
shigaboba <shigapovalrm@gmail.com>
2023-04-02 00:18:07 +0300

    Hw is done

--- src/main/java/com/example/exceptionhw/ExceptionhwApplication.java
+++ src/main/java/com/example/exceptionhw/ExceptionhwApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExceptionhwApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionhwApplication.class, args);
 	}
